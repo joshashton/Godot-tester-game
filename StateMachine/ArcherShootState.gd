26de2f74a028e56e723a937feeab4bd3b2ccf444
joshashton <joshua@ashtons.eu>
2024-01-16 23:43:06 +0000

    Tidy archer class

--- StateMachine/ArcherShootState.gd
+++ StateMachine/ArcherShootState.gd
@@ -11,93 +11,25 @@ var mouseLocation
 var shootPosition:String
 
 func Enter() -> void:
-	
 	print("ShootingState")
 	
-	#starting shooting location
-	print(archerFSM.startingShootPosition)
-	if archerFSM.startingShootPosition == "up":
-		spriteplayer.flip_h = false
-		animated_sprite.play("shootup")
-	elif archerFSM.startingShootPosition == "upside":
-		spriteplayer.flip_h = false
-		animated_sprite.play("shootupside")
-	elif archerFSM.startingShootPosition == "side":
-		spriteplayer.flip_h = false
-		animated_sprite.play("shootside")
-	elif archerFSM.startingShootPosition == "downside":
-		spriteplayer.flip_h = false
-		animated_sprite.play("shootdownside")
-	elif archerFSM.startingShootPosition == "down":
-		spriteplayer.flip_h = false
-		animated_sprite.play("shootdown")
-	elif archerFSM.startingShootPosition == "flipupside":
-		spriteplayer.flip_h = true
-		animated_sprite.play("shootupside")
-	elif archerFSM.startingShootPosition == "flipside":
-		spriteplayer.flip_h = true
-		animated_sprite.play("shootside")
-	elif archerFSM.startingShootPosition == "flipdownside":
-		spriteplayer.flip_h = true
-		animated_sprite.play("shootdownside")
-	elif archerFSM.startingShootPosition == "flipdown":
-		spriteplayer.flip_h = true
-		animated_sprite.play("shootdown")
-	else:
-		spriteplayer.flip_h = true
-		animated_sprite.play("shootup")
-	
-	#wait for anomation player to finish then return
-	
-	#Transitioned.emit(self, "Idle")
-	
-#switch statement
 func Exit() -> void:
-	pass
+	isBowCharged = false
 	
 func Update(delta):
-	
 	if isBowCharged && Input.is_action_just_released("left_click"):
 		#instantiate arrow
 		pass
 	
 	if (Input.is_action_pressed("left_click")):
-			
-		var attackMouse = character.get_local_mouse_position()
-		#print(attackMouse)	
-		mouseLocation = attackMouse
-		var angle = rad_to_deg(mouseLocation.angle())
-		
-		#update mouse location string
-		if (angle >= -90 && angle < -54):
-			shootPosition = "up"
-		elif (angle >= -54 && angle < -18):
-			shootPosition = "upside"
-		elif (angle >= -18 && angle < 18):
-			shootPosition = "side"
-		elif (angle >= 18 && angle < 54):
-			shootPosition = "downside"
-		elif(angle >= 54 && angle < 90):
-			shootPosition = "down"
-		elif (angle >= 90 && angle < 126):
-			shootPosition = "flipdown"
-		elif (angle >= 126 && angle < 162):
-			shootPosition = "flipdownside"
-		elif (angle >= -126 && angle < -90):
-			shootPosition = "flipup"
-		elif (angle >= -162 && angle < -126):
-			shootPosition = "flipupside"
-		else:
-			shootPosition = "flipside"
-		print(shootPosition)
+		#get mouse location when using bow
+		mouseLocation = character.get_local_mouse_position()
+		shootPosition = archerFSM.get_mouse_direction(mouseLocation)
 	
-		#isAiming(mouseLocation)
-		#print(mouseLocation)
-		#animated_sprite.seek()
-		#
 	elif(Input.is_action_just_released("left_click")):
 		print("mouse release")
 		Transitioned.emit(self, "Idle")
+		isBowCharged = false
 	
 func Unhandled_input(event):
 	pass
@@ -105,12 +37,11 @@ func Unhandled_input(event):
 func Physics_update(delta):
 	# Get the current position of the playback cursor
 	var current_frame = animated_sprite.get_current_animation_position()
+	if isBowCharged:
+		current_frame = 0.5
 	match shootPosition:
 		"up":
 			spriteplayer.flip_h = false
-			
-			# Set the new animation	    
-			# Seek to the same frame in the new animation
 			animated_sprite.play("shootup")
 		"upside":
 			spriteplayer.flip_h = false
@@ -139,6 +70,7 @@ func Physics_update(delta):
 		_:
 			spriteplayer.flip_h = true
 			animated_sprite.play("shootup")
+	#set animation to correct frame
 	animated_sprite.seek(current_frame)
 	
 func isAiming(mouseLocation):
